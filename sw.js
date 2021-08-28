f786cccca94da49d7943ef7406b4e66dc98cfac0
Gelford23 <gelfordfallan23@gmail.com>
2021-08-28 20:00:21 +0800

    newSw

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', e => {
-    console.log('Install');
-})
+self.addEventListener("install", e => {
+    e.waitUntil(
+      caches.open("static").then(cache => {
+        return cache.addAll(["./", "./src/style.css", "./images/Logo192.png"]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", e => {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return respose || fetch(e.request);
+      }))
+  })
